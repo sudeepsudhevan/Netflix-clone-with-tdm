a1cc8c4d7203c18566eb7268d710718730dc5319
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-02 19:19:25 +0530

    Navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import './NavBar.css'
+
 
 function NavBar() {
   return (
-    <div>
-        
+    <div className="navbar">
+      <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix" />
+      <img className='avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Avatar" />  
     </div>
   )
 }
