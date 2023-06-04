e6d487e465f4ede561a4d3b8a56165795104b616
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-06-04 19:52:50 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavBar from './Components/NavBar/NavBar';
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import RowPost from './Components/RowPost/RowPost';
+import { actionMovies, originals , comedyMovies , horrorMovies , romanceMovies , trending , topRated, documentaries } from './url';
+
 
 
 
@@ -11,7 +13,14 @@ function App() {
     <div className="App">
       <NavBar />
       <Banner />
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={trending} title='Trending' isSmall/>
+      <RowPost url={topRated} title='Top Rated' isSmall/>
+      <RowPost url={actionMovies} title='Action' isSmall/>
+      <RowPost url={comedyMovies} title='Comedy' isSmall/>
+      <RowPost url={horrorMovies} title='Horror' isSmall/>
+      <RowPost url={romanceMovies} title='Romance' isSmall/>
+      <RowPost url={documentaries} title='Documentaries' isSmall/>
     </div>
   );
 }
